dd87e89921945e40e89b17f8fd2ccefc9a58e913
Masahiro Nakagawa <repeatedly@gmail.com>
2014-01-14 19:58:31 +0900

    use_ssl is true by default in tdlog

--- lib/fluent/plugin/out_tdlog.rb
+++ lib/fluent/plugin/out_tdlog.rb
@@ -83,7 +83,7 @@ class TreasureDataLogOutput < BufferedOutput
     @record_size_limit = 32*1024*1024  # TODO
     @table_list = {}
     @auto_create_table = true
-    @use_ssl = false
+    @use_ssl = true
     @buffer_type = 'file'  # overwrite default buffer_type
     @flush_interval = 300  # overwrite default flush_interval to 5mins
     @empty_gz_data = create_empty_gz_data
