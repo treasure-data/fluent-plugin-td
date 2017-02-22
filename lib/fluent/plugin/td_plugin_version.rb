325b9e68e15a227fbec47c6bc13ed44a0e835449
Masahiro Nakagawa <repeatedly@gmail.com>
2017-02-23 08:40:29 +0900

    Fix version file

--- lib/fluent/plugin/td_plugin_version.rb
+++ lib/fluent/plugin/td_plugin_version.rb
@@ -1,7 +1,7 @@
-require 'fluent/plugin/output'
-
-module Fluent::Plugin
-  module TreasureDataPlugin
-    VERSION = '0.11.0.rc1'
+module Fluent
+  module Plugin
+    module TreasureDataPlugin
+      VERSION = '0.11.0.rc1'
+    end
   end
 end
